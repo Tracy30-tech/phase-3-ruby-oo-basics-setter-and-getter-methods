0990051dc8c92b20fb2455eaae4b882886cdbd90
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-02-13 12:22:11 +0300

    setter-getter-methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-
+    def name=(name)
+      @name = name
+      end 
+      
+    def breed=(name)
+      @breed = name
+    end  
+    
+    def name
+      @name
+    end 
+    
+    def breed
+      @breed
+     end 
+    
 end
+    
+
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    # setter method
+    def name=(name)
+      @name = name
+      end
 
+    def job=(name)
+      @job = name
+    end
+    
+      # getter method
+      def name
+        @name
+      end
+    
+      def job
+        @job
+      end
+    
 end
